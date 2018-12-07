02af8a60e2708afc2133821fcad5df0844de7811
Filip <filip.sokolowski@sofomo.com>
2018-12-07 12:14:58 +0100

    Updates

---
+++ precache-manifest.d0af003bb2df09ec0f40a3663c182be1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-service-worker/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a42ea63578895f74fea",
+    "url": "/test-service-worker/static/js/runtime~main.4a42ea63.js"
+  },
+  {
+    "revision": "03fa679d843da45b9bc8",
+    "url": "/test-service-worker/static/js/main.03fa679d.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/test-service-worker/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "03fa679d843da45b9bc8",
+    "url": "/test-service-worker/static/css/main.a1a4114e.chunk.css"
+  },
+  {
+    "revision": "673bc57ce5dcffd4263a22955d070865",
+    "url": "/test-service-worker/index.html"
+  }
+];
